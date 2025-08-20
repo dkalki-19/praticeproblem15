b94d86301521a5187e50409664c9b7c4ad65b627
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-20 14:28:20 +0530

    Updating with use case one

--- src/hashtableproblem/HashTableMain.java
+++ src/hashtableproblem/HashTableMain.java
@@ -3,6 +3,17 @@ package hashtableproblem;
 public class HashTableMain {
     public static void main(String[] args) {
         MyHashTable<String, Integer> hashTable = new MyHashTable<>(10);
+        
+     // UC1: Word frequency for sentence
+        String sentence = "To be or not to be";
+        String[] words = sentence.toLowerCase().split(" ");
 
+        for (String word : words) {
+            Integer count = hashTable.get(word);
+            if (count == null) count = 0;
+            hashTable.add(word, count + 1);
+        }
+
+        System.out.println("UC1 - Word Frequencies: " + hashTable);
     }
 }
