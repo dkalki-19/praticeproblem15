4770297558450bdcf3a0a231694f95596f741d7f
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-20 14:31:39 +0530

    Updating with usecase 2

--- src/hashtableproblem/HashTableMain.java
+++ src/hashtableproblem/HashTableMain.java
@@ -15,5 +15,22 @@ public class HashTableMain {
         }
 
         System.out.println("UC1 - Word Frequencies: " + hashTable);
+        
+        
+     // UC2: Paragraph frequency
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because "
+                + "they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] paraWords = paragraph.toLowerCase().split(" ");
+
+        MyHashTable<String, Integer> paraTable = new MyHashTable<>(20);
+
+        for (String word : paraWords) {
+            Integer count = paraTable.get(word);
+            if (count == null) count = 0;
+            paraTable.add(word, count + 1);
+        }
+
+        System.out.println("UC2 - Paragraph Frequencies: " + paraTable);
+
     }
 }
